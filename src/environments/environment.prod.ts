253eaa52f1b7f842b23957dc1c65fd1fd94b53b4
InteractiveTechHub <techprohub2025@gmail.com>
2025-03-07 21:04:16 -0300

    added uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBase: 'http://93.127.215.157',
-  apiUrl: 'http://93.127.215.157/api',
+  apiBase: 'http://jsdesenvolvimento.shop',
+  apiUrl: 'http://jsdesenvolvimento.shop/api',
   apiVersion: 'v1',
   environmentName: 'production'
 };
