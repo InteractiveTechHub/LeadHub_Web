0c26e8b05fb52acafd278a582ba5d6aec4de2f74
InteractiveTechHub <techprohub2025@gmail.com>
2025-03-16 18:43:40 -0300

    fixed path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: 'http://tecnoprohub.com/',
+  apiBase: 'http://tecnoprohub.com',
   apiUrl: 'http://tecnoprohub.com/api',
   apiVersion: 'v1',
   environmentName: 'production'
